c78031100116c420fae42a518120edac8aedba8f
Hryhorii Prokopiv <hryhorii@local.local>
2022-05-17 20:29:28 +0300

    checkpoint

--- src/main/scala/monad/Main.scala
+++ src/main/scala/monad/Main.scala
@@ -2,6 +2,24 @@ package monad
 
 object Main {
   @main def run(): Unit = {
-    println("foo")
+
+    CustomMonad("foo")
+      .flatMap(v => CustomMonad(v.toUpperCase))
+      .flatMap(v => CustomMonad(v.toLowerCase)) :: Nil foreach println
+
+
+
   }
+}
+/*
+*  Monad(x).flatMap(f) == f(x)
+*  Monad(x).flatMap(x => Monad(x)) == Monad(x)
+*  Monad(x).flatMap(f).flatMap(g) == Monad(x).flatMap(x => f(x).flatMap(g))
+*
+*
+* */
+case class CustomMonad[+T](private val value: T ) {
+  def get(): T = value
+  def flatMap[S](function: T => CustomMonad[S]): CustomMonad[S] =
+    function(value)
 }
