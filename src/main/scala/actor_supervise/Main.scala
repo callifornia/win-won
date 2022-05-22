70f089aff4fe68faada381dfc74b2d6c4d0fc6ae
Hryhorii Prokopiv <hryhorii@local.local>
2022-05-22 12:46:53 +0300

    checkpoint

---
+++ src/main/scala/actor_supervise/Main.scala
@@ -0,0 +1,6 @@
+package actor_supervise
+
+object Main {
+  @main def run(): Unit =
+    println("qwe")
+}
--- src/main/scala/actor_typed_pipe_to/Main.scala
+++ src/main/scala/actor_typed_pipe_to/Main.scala
@@ -23,7 +23,7 @@ object Main {
   }
 
   // PipeTo example as an actor
-  def someActor(counter: Int = 0, failure: Int = 0): Behavior[SomeMessage] =
+  def someActor(counter: Int = 0, failure: Int = 0): Behavior[SomeMessage] = {
     Behaviors.receive {
       (context, message) =>
         message match {
@@ -41,27 +41,31 @@ object Main {
             someActor(counter, failure + 1)
         }
     }
+  }
+
 
   // PipeTo example as a function
-    val someActor2: (Int, Int) => Behavior[SomeMessage] =
-      (counter, failure) =>
-        Behaviors.receive[SomeMessage] {
-          (context, message) =>
-            message match {
-              case FindPhone(name) =>
-                context.pipeToSelf(ExternalService.findPhone(name)) {
-                  case Success(phone) => PersonPhone(phone)
-                  case Failure(exception) => PhoneNotFound(name, exception)
-                }
-                Behaviors.same
-              case PersonPhone(number) =>
-                println(s"Founded persons number: [$number]")
-                someActor2(counter + 1, failure)
-              case PhoneNotFound(name, reason) =>
-                println(s"Persons [$name] number was not found. Reason: [$reason]")
-                someActor2(counter, failure + 1)
-            }
-        }
+  val someActor2: (Int, Int) => Behavior[SomeMessage] = {
+    (counter, failure) => {
+      Behaviors.receive[SomeMessage] {
+        (context, message) =>
+          message match {
+            case FindPhone(name) =>
+              context.pipeToSelf(ExternalService.findPhone(name)) {
+                case Success(phone) => PersonPhone(phone)
+                case Failure(exception) => PhoneNotFound(name, exception)
+              }
+              Behaviors.same
+            case PersonPhone(number) =>
+              println(s"Founded persons number: [$number]")
+              someActor2(counter + 1, failure)
+            case PhoneNotFound(name, reason) =>
+              println(s"Persons [$name] number was not found. Reason: [$reason]")
+              someActor2(counter, failure + 1)
+          }
+      }
+    }
+  }
 
   @main def run(): Unit = {
     val blahActor = ActorSystem(someActor2(0, 0), "asd")
