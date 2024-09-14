2444fcd89cda1601a94448cf21dae10494637d21
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-09-14 10:44:45 +0300

    checkpoint

---
+++ src/main/scala/repeat/main/scala/Main.scala
@@ -0,0 +1,19 @@
+package repeat.main.scala
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  /*  Algorithm Big O notation */
+  /*  Scala main  */
+      /*  Collections */
+      /*  Futures */
+  /*  Type classes */
+  /*  Cats  */
+  /*  Akka  */
+  /*  Kafka  */
+
+
+
+}
