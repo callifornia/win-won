0589d172262654fedef3f87b0912cac53b30bf61
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-09-14 14:34:08 +0300

    checkpoint

--- src/main/scala/repeat/main/scala/Main.scala
+++ src/main/scala/repeat/main/scala/Main.scala
@@ -31,6 +31,13 @@ object Main {
   /* https://www.youtube.com/watch?v=ZRdOb4yR0kk */
 
   /* Big O - показивает верхнюю межу складності виконання алгоритма в залежності від вхвідних параметрів. */
+  /*
+  * О(N^2 + N^2)            = O(N^2)
+  * О(N^2 + N)              = O(N^2)
+  * О(N + logN)             = O(N)
+  * O(5 * 2^N + 10*N^100)   = O(2^N)     2^N - растет гараздо бистрее чем N^100
+  * */
+
 
   /* Рекурсивна фунція яка рахує сумму чисел
   * У випадку коли N = 3 функція викличе себе 3 рази
@@ -55,15 +62,36 @@ object Main {
 
 
 
+  /*
+  * Швидкодія: O(A + B)
+  * */
+  def function_1(array_1: Array[Int], array_2: Array[Int]): Unit = {
+    array_1.foreach(println)
+    array_2.foreach(println)
+  }
+
+  /*
+  * Швидкодія: O(N^2) томущо є вложеність
+  * */
+  def function_2(array_1: Array[Int]): Unit = {
+    array_1.foreach(a =>
+      array_1.foreach(b =>
+        println(a + b)))
+  }
+
+  /*
+  * Швидкодія: O(A * B) томущо є вложеність
+  * */
+  def function_3(array_A: Array[Int], array_B: Array[Int]): Unit = {
+    array_A.foreach(a =>
+      array_B.foreach(b =>
+        println(a + b)))
+  }
 
 
-
-
-
-
-
-
-
+  /*
+    O(log N) -> для алгоритма, де на кожній ітерації береться половина елементів складність буде включати log N
+  */
 
 
 
