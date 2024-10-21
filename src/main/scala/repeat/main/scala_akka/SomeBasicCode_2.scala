c222c7f6a2b7e3c3a9ba3aab2734cc1126392a2d
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-10-21 21:02:48 +0300

    checkpoint

--- src/main/scala/repeat/main/scala_akka/SomeBasicCode_2.scala
+++ src/main/scala/repeat/main/scala_akka/SomeBasicCode_2.scala
@@ -9,37 +9,27 @@ import ShoppingCart._
 import scala.util.Random
 
 
-
 object SomeBasicCode_2 {
   def main(args: Array[String]): Unit = {
     println("Hi there ...")
-
-
-
-
-
+    val system = ActorSystem(rootBehavior(), "actor-system")
+    system ! "start"
   }
 
-  def rootBehavior(): Behavior[Any] =
-    Behaviors.setup[Any] { implicit context =>
-      val customerActor = context.spawn(CustomerActor.handle(), "custom-actor")
+
+  def rootBehavior(): Behavior[String] =
+    Behaviors.setup[String] {implicit context =>
       val shopingCartActor = context.spawn(ShoppingCartActor.handle(), "shopping-cart-actor")
+      val checkoutActor    = context.spawn(CheckoutActor.handle(shoppingCart = ShoppingCartActor(shopingCartActor)), "checkout-actor")
+      val customerActor    = context.spawn(CustomerActor.handle(), "custom-actor")
 
-      val checkoutActor = context.spawn(CheckoutActor.handle(
-        shoppingCart = ShoppingCartActor(shopingCartActor),
-        checkoutsInProgress = ???
-      ))
-
-
-      Behaviors.same[Any]
+      Behaviors.receiveMessage[String] {
+        case "start" =>
+          checkoutActor ! InspectSummary(StaticMockedData.cartId1, customerActor)
+          Behaviors.same[String]
+      }
     }
-
-
-
-
 }
-
-
 
 
 object RequestResponseWrappers {
@@ -51,101 +41,95 @@ object RequestResponseWrappers {
 
 
 object CustomerActor {
-  def handle(): Behavior[Any] =
-    Behaviors.receive[Any] { (context, msg) =>
+  def handle(): Behavior[Checkout.Response] =
+    Behaviors.receive[Checkout.Response] {(context, msg) =>
       msg match {
         case Checkout.Summary(cardId, amount) =>
           context.log.info(s"Total to pay: [${amount}] for cartId: [$cardId]")
-          Behaviors.same[Any]
+          Behaviors.same[Checkout.Response]
       }
     }
 }
+
 
 case class CustomerActor(ref: ActorRef[Checkout.Response])
 case class ShoppingCartActor(ref: ActorRef[ShoppingCart.Request])
 
 
 object CheckoutActor {
+
   import RequestResponseWrappers._
 
   def handle(shoppingCart: ShoppingCartActor,
-             checkoutsInProgress: Map[CardId, CustomerActor] = Map.empty)
-            (implicit context: ActorContext[Checkout.Request]): Behavior[Checkout.Request] = {
-    ///
-    Behaviors.receiveMessage[Checkout.Request] {
-      /*
-         Requests handling
-         message from customer - query the shopping cart, the recipient of that response is my message adapter
-      */
-      case InspectSummary(cartId, replyTo) =>
-        shoppingCart.ref ! ShoppingCart.GetCurrentCart(cartId, wrappedReplyTo)  /* <--- message adapter here */
-        handle(shoppingCart, checkoutsInProgress + (cartId -> replyTo))
+             checkoutsInProgress: Map[CardId, CustomerActor] = Map.empty): Behavior[Checkout.Request] =
+    Behaviors.receive[Checkout.Request] {
+      (context, msg) =>
+        msg match {
+          /*
+             Requests handling
+             message from customer - query the shopping cart, the recipient of that response is my message adapter
+          */
+          case InspectSummary(cartId, replyTo) =>
+            context.log.info(s"Checkout actor InspectSummary: [$cartId]")
+            shoppingCart.ref ! ShoppingCart.GetCurrentCart(cartId, wrappedReplyTo(context)) /* <--- message adapter here */
+            handle(shoppingCart, checkoutsInProgress + (cartId -> CustomerActor(replyTo)))
 
 
-      /*
-        Responses handling
-        the wrapped message from my adapter: deal with the Shopping Cart's response here
-      */
-      case WrappedStuff(CurrentCart(cartId, items)) =>
-        val summary = Summary(cartId, items.map(_.price).sum)
-        val customerActor = checkoutsInProgress(cartId)
-        customerActor.ref ! summary
-        Behaviors.same
+          /*
+            Responses handling
+            the wrapped message from my adapter: deal with the Shopping Cart's response here
+          */
+          case WrappedStuff(CurrentCart(cartId, items)) =>
+            context.log.info(s"Checkout actor WrappedStuff: [$cartId]")
+            val summary = Summary(cartId, items.map(_.price).sum)
+            val customerActor = checkoutsInProgress(cartId)
+            customerActor.ref ! summary
+            Behaviors.same
+        }
     }
-  }
 }
 
 
 object ShoppingCartActor extends StaticMockedData {
   def handle(): Behavior[ShoppingCart.Request] =
-    Behaviors.receiveMessage {
-      case GetCurrentCart(cartId, replyTo) =>
-        replyTo ! CurrentCart(cartId, mapa(cartId))
-        Behaviors.same[ShoppingCart.Request]
+    Behaviors.receive {
+      (contex, msg) =>
+        msg match {
+          case GetCurrentCart(cartId, replyTo) =>
+            contex.log.info("ShoppingCart actor GetCurrentCart")
+            replyTo ! CurrentCart(cartId, mapa(cartId))
+            Behaviors.same[ShoppingCart.Request]
+        }
     }
 }
 
 
-
-
-
-
-
-
-object StoreDomain{
+object StoreDomain {
   case class Product(name: String, price: Double)
   case class CardId(value: Int) extends AnyVal
 }
 
 
-object Customer {
-  sealed trait Request
-
-}
-
-
 object ShoppingCart {
   sealed trait Request
+
   case class GetCurrentCart(cartId: CardId, ref: ActorRef[Response]) extends Request
 
   sealed trait Response
+
   case class CurrentCart(cartId: CardId, items: List[Product]) extends Response
 }
 
 
-
 object Checkout {
   sealed trait Request
-  final case class InspectSummary(cardId: CardId, replyTo: ActorRef[Response]) extends Request
+
+  final case class InspectSummary(cardId: CardId, replyTo: ActorRef[Checkout.Response]) extends Request
 
   sealed trait Response
+
   final case class Summary(cartId: CardId, amount: Double) extends Response
 }
-
-
-
-
-
 
 
 trait StaticMockedData {
@@ -160,6 +144,8 @@ trait StaticMockedData {
 
   protected lazy val mapa = Map(cartId1 -> db, cartId2 -> db, cartId3 -> db)
 }
+
+object StaticMockedData extends StaticMockedData
 
 
 
