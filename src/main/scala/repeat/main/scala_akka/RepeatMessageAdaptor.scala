d7b1e96aea31589628f1a749dbf9b36598e29270
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-10-22 10:31:49 +0300

    checkpoint

---
+++ src/main/scala/repeat/main/scala_akka/RepeatMessageAdaptor.scala
@@ -0,0 +1,40 @@
+package repeat.main.scala_akka
+
+import akka.actor.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import repeat.main.scala_akka.Receiver.GetData
+
+object RepeatMessageAdaptor {
+  def main(args: Array[String]): Unit = {
+
+    val system = ActorSystem(rootBehavior(), "actor-system")
+    system ! "start"
+  }
+
+  object Customer {
+    trait Request
+  }
+  case class GetData(replyTo: ActorRef[Customer.Request]) extends Customer.Request
+  
+  def rootBehavior(): Behavior[String] =
+    Behaviors.setup[String] { context =>
+      val shoppingCartActor = context.spawn(shoppingCartBehavior(), "shopping-cart-behavior")
+      val checkoutActor = context.spawn(checkoutBehavior(shoppingCartActor), "checkout-behavior")
+      val customerActor = context.spawn(customerBehavior(), "customer-actor")
+
+      Behaviors.receive {
+        (context, msg) =>
+          msg match {
+            case "start" =>
+              checkoutActor ! GetData(replyTo = customerActor)
+          }
+      }
+
+      Behaviors.same[String]
+    }
+
+
+}
+
+
