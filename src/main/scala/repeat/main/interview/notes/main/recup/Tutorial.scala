45465c16f47c39bdfb7d81df25fec52046254102
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-12-02 13:17:06 +0200

    checkpoint

--- src/main/scala/repeat/main/interview/notes/main/recup/Tutorial.scala
+++ src/main/scala/repeat/main/interview/notes/main/recup/Tutorial.scala
@@ -4,6 +4,7 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
+
 object Tutorial {
 
 
@@ -11,7 +12,7 @@ object Tutorial {
 
   /* Monad Option */
   import cats.Monad
-  import cats.instances.option._
+//  import cats.instances.option._
 
   val anOptionMonad = Monad[Option]
   val anOptionValue = anOptionMonad.pure(123)
@@ -32,10 +33,23 @@ object Tutorial {
   val aFutureResult = aFutureMonad.flatMap(aFutureValue)(x => Future(x * 2))
   aFutureResult.onComplete(println)
 
+  def listPair(numbers: List[Int], chars: List[Char]): List[(Int, Char)] = numbers.flatMap(x => chars.map(c => (x, c)))
+  def optionPair(numbers: Option[Int], chars: Option[Char]): Option[(Int, Char)] = numbers.flatMap(x => chars.map(c => (x, c)))
+  def futurePair(numbers: Future[Int], chars: Future[Char]): Future[(Int, Char)] = numbers.flatMap(x => chars.map(c => (x, c)))
+
+  def calculate[M[_], A, B](m1: M[A], m2: M[B])(implicit v: Monad[M]): M[(A, B)] =
+    Monad[M].flatMap(m1)(x => Monad[M].map(m2)(y => (x, y)))
+
+  /*  right way   */
+  def calculate2[M[_], A, B](m1: M[A], m2: M[B])(implicit monad: Monad[M]): M[(A, B)] =
+    monad.flatMap(m1)(x => monad.map(m2)(y => (x, y)))
 
 
 
-
+//  def main(args: Array[String]): Unit = {
+//    println("====>" + calculate(List(1,2,3), List('a', 'b', 'c')))
+//    println("====>" + calculate(Option(1), Option('a')))
+//  }
 
 
 
