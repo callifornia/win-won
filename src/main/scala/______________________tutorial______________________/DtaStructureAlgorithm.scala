d6a1b01c52cafad7e762b1a0353609f05e51de41
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-12-12 15:31:00 +0200

    checkpoint

---
+++ src/main/scala/______________________tutorial______________________/DtaStructureAlgorithm.scala
@@ -0,0 +1,35 @@
+package ______________________tutorial______________________
+
+object DtaStructureAlgorithm {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+  /*
+
+       ######## DataStructure and Algorithm ########
+
+   Big O notation
+
+          |   O(n^2)   O(n^2)
+          |     .      .               O (n log(n))          O(n^2)
+          |     .     .                 - quickSort            - insertSort
+          |     .    .                  - mergeSort            - selectionSor
+          |     .   .                   - heapSort             - bubbleSort
+          |    .  .
+          |   . .
+          |  .
+          |_______________________
+
+
+
+
+
+
+
+
+
+  * */
+
+}
--- src/main/scala/______________________tutorial______________________/poligon/Poligon.scala
+++ src/main/scala/______________________tutorial______________________/poligon/Poligon.scala
@@ -1,7 +1,37 @@
 package ______________________tutorial______________________.poligon
 
+import scala.collection.mutable.ArrayBuffer
+
 object Poligon {
   def main(args: Array[String]): Unit = {
-    println("foo....")
+    //    val array: ArrayBuffer[Int] = Random.shuffle(ArrayBuffer(1,2,3,4,5,6,7,8,9,10))
+    val array: ArrayBuffer[Int] = ArrayBuffer(8, 2, 4, 7, 1, 3, 9, 6, 5)
+    println("Init array: " + array)
+    println(quickSort(array))
+  }
+
+
+  //  (8,2,4,7,1,3,9,6,5)
+  def quickSort(array: ArrayBuffer[Int]): ArrayBuffer[Int] = {
+    var pivotI = array.length - 1
+    var pivot = array(pivotI)
+    println("pivot: " + pivot)
+
+    var indexI = 0
+    var indexJ = indexI + 1
+
+    while (indexJ <= array.length) {
+      if (array(indexJ) < pivot) {
+        val tmp = array(indexI)
+        array.update(indexI, array(indexJ))
+        array.update(indexJ, tmp)
+        indexI = indexI + 1
+        indexJ = indexJ + 1
+      } else {
+        indexJ = indexJ + 1
+      }
+    }
+
+    array
   }
 }
