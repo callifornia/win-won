0dca9acd868638fc9e787cc72b5dbfc458b11257
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-12-12 20:44:56 +0200

    checkpoint

--- src/main/scala/______________________tutorial______________________/DtaStructureAlgorithm.scala
+++ src/main/scala/______________________tutorial______________________/DtaStructureAlgorithm.scala
@@ -41,33 +41,47 @@ object DtaStructureAlgorithm {
 
 
   // QuickSort
-
   def main(args: Array[String]): Unit = {
-
-    val array: ArrayBuffer[Int] = Random.shuffle(ArrayBuffer.from((1 to 10)))
-    println("Init array: " + array.mkString(","))
-    val result = quickSort(array, start = 0, end = array.length - 1)
-    println("Result: " + result)
+    val array: ArrayBuffer[Int] = Random.shuffle(ArrayBuffer.from((1 to 9)))
+    println("Init  : " + array.mkString(" "))
+    quickSort2(array, start = 0, end = array.length - 1)
+    println("Result: " + array.mkString(" "))
+    assert(array == ArrayBuffer(1,2,3,4,5,6,7,8,9))
   }
 
-
-  def quickSort(array: ArrayBuffer[Int], start: Int, end: Int): Unit =
+  def quickSort2(array: ArrayBuffer[Int], start: Int, end: Int): Unit =
     if (start <= end) {
-      println(s"start: $start  end: $end")
-      val pivot = partition(array, start, end)
-      println(pivot)
+      val pivot = partition2(array, start, end)
 
-      quickSort(array, start, pivot - 1)
-      quickSort(array, pivot + 1, end)
+      quickSort2(array, start, pivot - 1)
+      quickSort2(array, pivot + 1, end)
     }
+
+
+  def partition2(array: ArrayBuffer[Int], start: Int, end: Int): Int = {
+    val pivot = array(end)
+    var i = start - 1
+    (start to end - 1).foreach { j =>
+      if (array(j) <= pivot) {
+        i = i + 1
+        val tmp = array(i)
+        array.update(i, array(j))
+        array.update(j, tmp)
+      }
+    }
+    i = i + 1
+    val tmp = array(i)
+    array.update(i, array(end))
+    array.update(end, tmp)
+    i
+  }
+
 
 
   def partition(array: ArrayBuffer[Int], start: Int, end: Int): Int = {
     val pivot = array(end)
-    println("array: " + array)
-    println("pivot: " + pivot)
     var i = start - 1
-    Range(start, end - 1).foreach{ j =>
+    (start to end - 1).foreach { j =>
       if (array(j) < pivot) {
         i = i + 1
         val tmp = array(i)
@@ -75,10 +89,12 @@ object DtaStructureAlgorithm {
         array.update(j, tmp)
       }
     }
+    i = i + 1
+    val tmp = array(i)
+    array.update(i, array(end))
+    array.update(end, tmp)
     i
   }
-
-
 
 
 
