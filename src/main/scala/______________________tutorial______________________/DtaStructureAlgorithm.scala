c56b7bbd0c74de1d18123c4509fe6e3265a1bca0
Prokopiv Hryhorii <prokopivhryhorii@Prokopivs-MacBook-Pro.local>
2024-12-12 16:32:36 +0200

    checkpoint

--- src/main/scala/______________________tutorial______________________/DtaStructureAlgorithm.scala
+++ src/main/scala/______________________tutorial______________________/DtaStructureAlgorithm.scala
@@ -1,10 +1,9 @@
 package ______________________tutorial______________________
 
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
 object DtaStructureAlgorithm {
-  def main(args: Array[String]): Unit = {
-
-  }
-
 
   /*
 
@@ -38,17 +37,49 @@ object DtaStructureAlgorithm {
           |_______________________________
 
 
-
-
-
-
-
-
-
-
-
-
-
-          
    */
+
+
+  // QuickSort
+
+  def main(args: Array[String]): Unit = {
+
+    val array: ArrayBuffer[Int] = Random.shuffle(ArrayBuffer.from((1 to 10)))
+    println("Init array: " + array.mkString(","))
+    val result = quickSort(array, start = 0, end = array.length - 1)
+    println("Result: " + result)
+  }
+
+
+  def quickSort(array: ArrayBuffer[Int], start: Int, end: Int): Unit =
+    if (start <= end) {
+      println(s"start: $start  end: $end")
+      val pivot = partition(array, start, end)
+      println(pivot)
+
+      quickSort(array, start, pivot - 1)
+      quickSort(array, pivot + 1, end)
+    }
+
+
+  def partition(array: ArrayBuffer[Int], start: Int, end: Int): Int = {
+    val pivot = array(end)
+    println("array: " + array)
+    println("pivot: " + pivot)
+    var i = start - 1
+    Range(start, end - 1).foreach{ j =>
+      if (array(j) < pivot) {
+        i = i + 1
+        val tmp = array(i)
+        array.update(i, array(j))
+        array.update(j, tmp)
+      }
+    }
+    i
+  }
+
+
+
+
+
 }
