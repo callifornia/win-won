ff6dd58a2d2a52a6dde64d059130a03283a56e3d
Hryhorii Prokopiv <hryhorii@Hryhoriis-MacBook-Pro.local>
2026-07-21 18:13:36 +0300

    checkpoint

--- src/main/scala/sparkk/SparkExercises.scala
+++ src/main/scala/sparkk/SparkExercises.scala
@@ -1,20 +1,79 @@
 package sparkk
 
+import org.apache.spark.sql.types.StringType
 import util.InitSession._
-import util.Spark._
+
+import java.util.regex.Pattern
+//import util.Spark._
+import spark.implicits._
+import org.apache.spark.sql.functions._
+
+
 
 object SparkExercises {
 
   def main(args: Array[String]): Unit = {
-    SparkWorkshop.exercises_1
+    SparkWorkshop.exercises_2()
   }
 
 
   object SparkWorkshop {
 
-    //  split function with variable delimiter per row
+    // selecting the most important rows per assigned priority ...
+    def exercises_2(): Unit = {
+      val input = Seq(
+        (1, "MV1"),
+        (1, "MV2"),
+        (2, "VPV"),
+        (2, "Others")).toDF("id", "value")
+
+      input.show()
+      input.groupBy($"id").
+    }
+
+
+    //  split function with variable delimiter per row ...
     def exercises_1(): Unit = {
-      println("foo ...")
+
+      val dept = Seq(
+        ("50000.0#0#0#", "#"),
+        ("0@1000.0@", "@"),
+        ("1$", "$"),
+        ("1000.00^Test_string", "^"),
+        ("", "^"),
+        ("a^", "^")
+      ).toDF("VALUES", "Delimiter")
+
+      val quoteRegex = udf { (str: String) => Pattern.quote(str) }
+
+      // via UDF
+      dept
+        .withColumn("split_values", split($"VALUES", quoteRegex($"Delimiter")))
+        .withColumn("extra",        filter($"split_values", _ =!= ""))
+
+      dept
+        .withColumn("split_values", split($"VALUES", quoteRegex($"Delimiter")))
+        .withColumn("extra",        array_remove($"split_values", ""))
+
+
+      // other ...
+      dept
+        .withColumn("split_values", array_remove(split($"VALUES", concat(lit("\\").cast(StringType), $"Delimiter")), ""))
+        .show(truncate = false)
+
+
+      // tutorial solution
+      dept
+        .withColumn("split_values", expr("""split(values, concat("\\", delimiter))"""))
+        .withColumn("extra",        array_remove('split_values, "")).show(truncate = false)
     }
   }
 }
+
+
+
+
+
+
+
+
