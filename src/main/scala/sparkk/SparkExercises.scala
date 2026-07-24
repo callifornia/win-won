f407fc92b5794d7eab53774d10ca9c1fe143d8c6
Hryhorii Prokopiv <hryhorii@Hryhoriis-MacBook-Pro.local>
2026-07-24 13:21:42 +0300

    checkpoint

--- src/main/scala/sparkk/SparkExercises.scala
+++ src/main/scala/sparkk/SparkExercises.scala
@@ -1,5 +1,6 @@
 package sparkk
 
+import org.apache.spark.sql.classic.DataFrame
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.types.StringType
 import util.InitSession._
@@ -11,13 +12,144 @@ import org.apache.spark.sql.functions._
 
 
 object SparkExercises {
+  case class MovieRatings(movieName: String, rating: Double)
+
+  case class MovieCritics(name: String, movieRatings: Seq[MovieRatings])
 
   def main(args: Array[String]): Unit = {
-    SparkWorkshop.exercise_4()
+    SparkWorkshop.exercise_5_1()
   }
 
 
   object SparkWorkshop {
+
+    // structs for column names and values
+    /* with explode and group by and pivot */
+    def exercise_5_1(): Unit = {
+      val data = Seq(
+        MovieCritics("Manuel", Seq(MovieRatings("Logan", 1.5), MovieRatings("Zoolander", 3), MovieRatings("John Wick", 2.5))),
+        MovieCritics("John", Seq(MovieRatings("Logan", 2), MovieRatings("Zoolander", 3.5), MovieRatings("John Wick", 3))))
+
+      val df = data.toDF()
+      df
+        .withColumn("movieRatingExploded", explode($"movieRatings"))
+        .withColumn("moviName", $"movieRatingExploded.movieName") //  .select($"name", $"exploded.*")
+        .withColumn("rating", $"movieRatingExploded.rating")
+        .groupBy("name")
+        .pivot("moviName")
+        .agg(first("rating"))
+        .show()
+    }
+
+    /* without group by and pivot */
+    def exercise_5_2(): Unit = {
+      val data = Seq(
+        MovieCritics("Manuel", Seq(MovieRatings("Logan", 1.5), MovieRatings("Zoolander", 3), MovieRatings("John Wick", 2.5))),
+        MovieCritics("John", Seq(MovieRatings("Logan", 2), MovieRatings("Zoolander", 3.5), MovieRatings("John Wick", 3))))
+      val df = data.toDF()
+      /*
+        +------+--------------------------------------------------+
+        |name  |movieRatings                                      |
+        +------+--------------------------------------------------+
+        |Manuel|[{Logan, 1.5}, {Zoolander, 3.0}, {John Wick, 2.5}]|
+        |John  |[{Logan, 2.0}, {Zoolander, 3.5}, {John Wick, 3.0}]|
+        +------+--------------------------------------------------+
+      * */
+
+      val ratingSize: Int = df
+        .withColumn("size", size($"movieRatings"))
+        .select(max("size"))
+        .as[Int]
+        .head() // 3
+
+      /*
+        {Logan, 1.5}, {Zoolander, 3.0}, {John Wick, 2.5}
+        size 3
+      */
+
+      // fetch values for each column by index (previous step)
+      val all = (0 until ratingSize).foldLeft(df) {
+        (ds, count) =>
+          ds
+            .withColumn(s"movie_name_$count", $"movieRatings"(count)("movieName"))
+            .withColumn(s"rating_$count", $"movieRatings"(count)("rating"))
+      }
+
+      /*
+      * +------+--------------------------------------------------+------------+--------+------------+--------+------------+--------+
+        |name  |movieRatings                                      |movie_name_0|rating_0|movie_name_1|rating_1|movie_name_2|rating_2|
+        +------+--------------------------------------------------+------------+--------+------------+--------+------------+--------+
+        |Manuel|[{Logan, 1.5}, {Zoolander, 3.0}, {John Wick, 2.5}]|Logan       |1.5     |Zoolander   |3.0     |John Wick   |2.5     |
+        |John  |[{Logan, 2.0}, {Zoolander, 3.5}, {John Wick, 3.0}]|Logan       |2.0     |Zoolander   |3.5     |John Wick   |3.0     |
+        +------+--------------------------------------------------+------------+--------+------------+--------+------------+--------+
+      * */
+
+      all.show(truncate = false)
+      val movieNameColumns = all.columns.filter(_.startsWith("movie_name")).map(col)
+      val movieNameValues = all.select(movieNameColumns: _*).head().toSeq.map(_.toString)
+      val ratingNameColumns = movieNameColumns.indices.map(i => s"rating_$i")
+
+      /*
+         movie column names are:    movie_name_0,movie_name_1,movie_name_2
+         movie column values are:   Logan, Zoolander, John Wick
+         ratingNameColumns:         rating_0,rating_1,rating_2
+      * */
+
+      /*  col(rating_0) as Logan      */
+      /*  col(rating_1) as Zoolander  */
+      /*  col(rating_2) as John Wick  */
+      val movieWithRatingColumns = movieNameValues.zip(ratingNameColumns).map {
+        case (movieName, ratingColumnName) => col(ratingColumnName) as movieName
+      }
+
+
+      val baseColumns = "name" :: "movieRatings" :: Nil map col
+      val result = all.select((baseColumns ++ movieWithRatingColumns): _*)
+      result.show(truncate = false)
+
+    }
+
+
+    /* solution */
+    def exercise_5_3(): Unit = {
+
+      val data = Seq(
+        MovieCritics("Manuel", Seq(MovieRatings("Logan", 1.5), MovieRatings("Zoolander", 3), MovieRatings("John Wick", 2.5))),
+        MovieCritics("John", Seq(MovieRatings("Logan", 2), MovieRatings("Zoolander", 3.5), MovieRatings("John Wick", 3))))
+
+      val ratings = data.toDF()
+      val ratingsCount =
+        ratings
+          .withColumn("size", size($"movieRatings"))
+          .select(max("size"))
+          .as[Int]
+          .head
+
+      val names_ratings =
+        (0 until ratingsCount)
+          .foldLeft(ratings) { case (ds, counter) =>
+            ds
+              .withColumn(s"name_$counter", $"movieRatings"(counter)("movieName"))
+              .withColumn(s"rating_$counter", $"movieRatings"(counter)("rating"))
+          }
+
+      val movieColumns =
+        names_ratings
+          .columns
+          .drop(1)
+          .filter(name => name.startsWith("name"))
+          .map(col)
+
+      val movieNames = names_ratings.select(movieColumns: _*).head.toSeq.map(_.toString)
+
+      val ratingNames = movieNames.indices.map(idx => s"rating_$idx")
+      val cols = movieNames.zip(ratingNames).map { case (movie, rn) =>
+        col(rn) as movie
+      }
+      names_ratings.select(($"name" +: cols): _*).show(truncate = false)
+    }
+
+
 
     // limiting collect_set Standard Function
     def exercise_4(): Unit = {
@@ -26,34 +158,36 @@ object SparkExercises {
       df.show(100)
       df
         .groupBy("key")
-        .agg(collect_list("id").as("all"))
+        .agg(sort_array(collect_set("id")).as("all"))
+        .withColumn("first three elements", slice($"all", 1, 3))
         .sort($"key")
         .show(truncate = false)
     }
 
+    
     // adding count to the source DataFrame
     def exercise_3(): Unit = {
       val df = Seq(
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001",  2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2),
-        ("05:49:56.604908", "10.0.0.3.5001",  "10.0.0.2.54880", 2)).toDF("column0", "column1", "column2", "label")
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604900", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604899", "10.0.0.2.54880", "10.0.0.3.5001", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2),
+        ("05:49:56.604908", "10.0.0.3.5001", "10.0.0.2.54880", 2)).toDF("column0", "column1", "column2", "label")
 
       val windowFunction = Window.partitionBy("column1")
 
@@ -97,7 +231,7 @@ object SparkExercises {
       // via UDF
       dept
         .withColumn("split_values", split($"VALUES", quoteRegex($"Delimiter")))
-        .withColumn("extra",        filter($"split_values", _ =!= ""))
+        .withColumn("extra", filter($"split_values", _ =!= ""))
 
       // other ...
       dept
@@ -108,7 +242,7 @@ object SparkExercises {
       // tutorial solution
       dept
         .withColumn("split_values", expr("""split(values, concat("\\", delimiter))"""))
-        .withColumn("extra",        array_remove('split_values, "")).show(truncate = false)
+        .withColumn("extra", array_remove('split_values, "")).show(truncate = false)
     }
   }
 }
